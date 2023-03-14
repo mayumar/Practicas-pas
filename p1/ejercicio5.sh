7ebb44eb448f6a3ec4398a295053a2b572d2d3aa
mayumar <maytalbada@gmail.com>
2023-03-14 13:16:21 +0100

    Ejercicio 5 p1 terminado

--- p1/ejercicio5.sh
+++ p1/ejercicio5.sh
@@ -10,11 +10,33 @@ fi
 directorio=$1
 tam=$2
 
-cd $directorio
-
 if !([ -d $directorio ]);
 then
     echo "$directorio no es un directorio"
     exit
 fi
 
+if [ $tam -le 0 ];
+then
+    echo "El numero de bytes debe ser positivo"
+    exit
+fi
+
+for fichero in $(find $directorio -size "+"$tam"c");
+do
+    if !([ -d $fichero ]);
+    then
+        let ejecutable=0
+        if [ -x $fichero ];
+        then
+            let ejecutable=1
+        fi
+        echo -e "$(basename $fichero); $(stat --print=%w $fichero); $(stat --print=%s $fichero); $(stat --print=%A $fichero); $ejecutable" >> f1
+    fi
+done
+
+sort -o f1 -t";" -k3nr -u f1
+
+echo "Nombre, FechaModificacion, Tamano, Permisos, Ejecutable"
+cat f1
+rm f1
